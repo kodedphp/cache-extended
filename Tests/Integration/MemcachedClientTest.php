a957cead340ad4f005bb80f0991babccee0c4653
Mihail Binev <kodeart@gmail.com>
2019-05-20 16:21:15 +0200

    - removes the CI environment check

--- Tests/Integration/MemcachedClientTest.php
+++ Tests/Integration/MemcachedClientTest.php
@@ -13,15 +13,9 @@ class MemcachedClientTest extends CachePoolIntegrationTest
      */
     public function createCachePool()
     {
-        if (getenv('CI')) {
-            $servers = [['127.0.0.1', 11211]];
-        } else {
-            $servers = [["memcached", 11211]];
-        }
-
         return CachePool::use('redis', [
             'host'    => getenv('REDIS_SERVER_HOST'),
-            'servers' => $servers,
+            'servers' => [["memcached", 11211]],
         ]);
     }
 
